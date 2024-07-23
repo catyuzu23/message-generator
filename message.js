3e17f3a4af6c160b8eca58a6f3d631148d4fac96
catyuzu23 <cfruja@gmail.com>
2024-07-23 16:54:07 +0300

    implemented first component for the message

--- message.js
+++ message.js
@@ -1,4 +1,5 @@
 //The project will take a number between 0 and 100 and will tell you if it is a good number for you or a bad one
 
 let x = Math.floor(Math.random() * 100);
-console.log(x);
+let nume=["good","bad"];
+console.log(`The number ${x} is ${nume[Math.floor(Math.random() * 2)]} for you.`);
